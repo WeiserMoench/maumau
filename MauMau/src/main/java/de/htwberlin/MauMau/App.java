d2768b19b807c823938e5c76e71242ebb9df317a
Studium <Studium@141.45.36.203>
2018-10-22 10:09:14 +0200

    App.java aufgeraeumt, denn da war noch quatsch drin

--- MauMau/src/main/java/de/htwberlin/MauMau/App.java
+++ MauMau/src/main/java/de/htwberlin/MauMau/App.java
@@ -13,10 +13,6 @@ public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Git ist scheisse....!" );
-        System.out.println("hoffe nicht wir...");
-        System.out.println("Eclipse ist trotzdem scheisse");
-        System.out.println("Gruesse aus IntelliJ");
         log.warn("end of Main");
     }
 }
