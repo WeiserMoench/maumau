6f61b1fafd05c9b43a3b8273c2ab28b34c1ad1ec
Joerg <Joerg@141.45.47.151>
2018-10-22 11:48:29 +0200

    Services überarbeitet

--- MauMau/src/main/java/de/htwberlin/MauMau/services/services.java
+++ MauMau/src/main/java/de/htwberlin/MauMau/services/services.java
@@ -1,24 +1,35 @@
 package de.htwberlin.MauMau.services;
 
+import de.htwberlin.MauMau.model.Kartendeck;
+import de.htwberlin.MauMau.model.Spiel;
+import de.htwberlin.MauMau.model.Spieler;
+
 /**
+ * 
  * @author Joerg Lehmann, Christian Fiebelkorn, Dustin Lange
  * Diese Datei enthaelt alle Methoden, die fuer den Spielablauf noetig sind.
  */
 public interface services {
-    void anlegenKarte();
+//	public void anlegenKarte();
+	
+//	public void anelgenKartendeck();
+	
+	public void auswaehlenKartendeck(Spiel spiel, Kartendeck kartendeck);
+	
+	public Spiel anlegenNeuesSpiel();
+	
+	public void auswaehlenSpielerFuerSpiel(Spiel spiel);
 
-    void anelgenKartendeck();
-
-    void anlegenNeuerSpieler();
-
-    void anlegenNeuesSpiel();
-
-    void mischenKartendeck();
-
-    void austeilenKartenAnSpieler();
-
-    void ziehenKarteVomZiehstapel();
-
-    void legenKarteAufAblageStapel();
-
+	public void mischenKartendeck(Kartendeck kartendeck);
+	
+	public void ziehenKarteVomZiehstapel(int anzahlKarten, Spiel spiel, Spieler spieler);
+	
+	public void legenKarteAufAblageStapel(Spiel spiel, Spieler spieler);
+	
+	public void aendernAnzahlZiehkarten(Spiel spiel);
+	
+	public void aendernSpielrichtung(Spiel spiel);
+	
+	
+	
 }
