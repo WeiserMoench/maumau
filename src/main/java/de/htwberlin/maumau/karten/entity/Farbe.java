5e2294e7884cd2569e1b5ff9eb7064668d87e191
Dustin <dustin.lange94@gmail.com>
2019-01-04 19:31:00 +0100

    Farben zu den Symbolen hinzugefuegt

--- src/main/java/de/htwberlin/maumau/karten/entity/Farbe.java
+++ src/main/java/de/htwberlin/maumau/karten/entity/Farbe.java
@@ -6,21 +6,40 @@
 package de.htwberlin.maumau.karten.entity;
 
 public enum Farbe {
+
+
+
     HERZ {
         public String toString() {
-            return Character.toString('\u2665');
+            final String ANSI_RESET = "\u001B[0m";
+            final String ANSI_RED = "\u001B[31m";
+            String c = Character.toString('\u2665');
+            String symbol = ANSI_RED + c + ANSI_RESET;
+            return symbol;
         }
     }, PIK {
         public String toString() {
-            return Character.toString('\u2660');
+            final String ANSI_RESET = "\u001B[0m";
+            final String ANSI_GREEN = "\u001B[32m";
+            String c = Character.toString('\u2660');
+            String symbol = ANSI_GREEN + c + ANSI_RESET;
+            return symbol;
         }
     }, KREUZ {
         public String toString() {
-            return Character.toString('\u2663');
+            final String ANSI_RESET = "\u001B[0m";
+            final String ANSI_PURPLE = "\u001B[35m";
+            String c = Character.toString('\u2663');
+            String symbol = ANSI_PURPLE + c + ANSI_RESET;
+            return symbol;
         }
     }, KARO {
         public String toString() {
-            return Character.toString('\u2666');
+            final String ANSI_RESET = "\u001B[0m";
+            final String ANSI_BLUE = "\u001B[34m";
+            String c = Character.toString('\u2666');
+            String symbol = ANSI_BLUE + c + ANSI_RESET;
+            return symbol;
         }
     }
 }
